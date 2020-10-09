1883dce3a63384f96d78a1b2aa200b073d2ef888
TogoFire <italomellopereira@gmail.com>
2020-10-09 00:33:23 -0300

    rosy: Build keymaster 4.0 and add it to hidl entry
    I hwservicemanager: getTransport: Cannot find entry android.hardware.keymaster@4.0::IKeymasterDevice/default in either framework or device VINTF manifest.
    
    Signed-off-by: TogoFire <italomellopereira@gmail.com>
    Signed-off-by: Aditya Eka Pranata <aepranata@gmail.com>

--- device.mk
+++ device.mk
@@ -260,9 +260,9 @@ PRODUCT_COPY_FILES += \
 
 # Keymaster HAL
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0-impl \
-    android.hardware.keymaster@3.0-service \
-    android.hardware.keymaster@3.0.vendor
+    android.hardware.keymaster@4.0-impl \
+    android.hardware.keymaster@4.0-service \
+    android.hardware.keymaster@4.0.vendor
 
 # Light
 PRODUCT_PACKAGES += \
