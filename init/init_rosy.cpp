11ad138af4e3ca5597c8a9548ff715bc3fd76baa
Jabiyeff <cebiyevanar@gmail.com>
2021-08-11 22:15:50 +0400

    rosy: init: Enable low_ram flag for 2GB RAM version
    * So much report available for multitasking issue
    
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>
    Signed-off-by: Aditya Eka Pranata <aepranata@gmail.com>

--- init/init_rosy.cpp
+++ init/init_rosy.cpp
@@ -94,7 +94,9 @@ void set_avoid_gfxaccel_config() {
     struct sysinfo sys;
     sysinfo(&sys);
 
-    if (sys.totalram <= 3072ull * 1024 * 1024) {
+    if (sys.totalram <= 2048ull * 1024 * 1024) {
+        // Generated from build/make/target/product/go_defaults_common.mk
+        property_override("ro.config.low_ram", "true");
         // Reduce memory footprint
         property_override("ro.config.avoid_gfx_accel", "true");
     }
