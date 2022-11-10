a3bc035f237d3d5b5304590970473e2f9ab369bc
aepranata <aep.repository@gmail.com>
2022-11-10 12:15:26 +0700

    rosy: Build Gallery package
    Since on RiceDroid Vanilla doesn't have gallery package, we build it from source

--- device.mk
+++ device.mk
@@ -205,6 +205,10 @@ PRODUCT_PACKAGES += \
     libvndfwk_detect_jni.qti \
     libvndfwk_detect_jni.qti.vendor
 
+# Gallery
+PRODUCT_PACKAGES += \
+    Gallery2
+
 # Gatekeeper HAL
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
