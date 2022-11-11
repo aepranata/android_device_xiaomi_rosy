05ab1b68f7918842441a2a48ea34e449e98c8517
aepranata <aep.repository@gmail.com>
2022-11-12 00:32:34 +0700

    rosy: kernel: Add some flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -50,6 +50,9 @@ TARGET_KERNEL_CONFIG := rosy-perf_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/rosy
 TARGET_KERNEL_NEW_GCC_COMPILE := true
 TARGET_KERNEL_CLANG_COMPILE := false
+KERNEL_MAKE_FLAGS += CPATH="/usr/include:/usr/include/x86_64-linux-gnu"
+HOSTCFLAGS="-fuse-ld=lld"
+HOSTLDFLAGS="-L/usr/lib/x86_64-linux-gnu -L/usr/lib64 -fuse-ld=lld"
 
 # ANT
 BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
