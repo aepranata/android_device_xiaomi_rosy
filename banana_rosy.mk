3a91c7bb75fd3abaf022444cf093c060204edd38
aepranata <aep.repository@gmail.com>
2022-12-22 18:49:10 +0700

    rosy: Change BananaDroid maintainer

--- banana_rosy.mk
+++ banana_rosy.mk
@@ -13,7 +13,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk
 $(call inherit-product, vendor/banana/config/common.mk)
 
 TARGET_BOOT_ANIMATION_RES := 720
-BANANA_MAINTAINER := A³-Project
+BANANA_MAINTAINER := aepranata
 TARGET_FACE_UNLOCK_SUPPORTED := true
 
 # Inherit from rosy device
